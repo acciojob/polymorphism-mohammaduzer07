40382c51a78de7049059bfd3c8161e9f895038e5
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2023-12-13 00:55:54 +0530

    Polymorphism assingment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,28 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        int p1 = p.product(10, 20);
+        int p2 = p.product(10,20,30);
+        double p3 = p.product(20,25);
+
+    }
+    public static class Product{
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+
+    }
+
 }
